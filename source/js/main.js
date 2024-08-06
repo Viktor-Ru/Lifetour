7f045a57a850d8430767ed465603bddd04d7102a
viktor <viktor-876@yandex.ru>
2024-08-06 16:54:20 +0300

    clear

--- source/js/main.js
+++
@@ -1,8 +0,0 @@
-import { initMainMenu } from './menu.js';
-import { initHeroSlider, initToursSlider, initTrainingSlider } from './sliders.js';
-
-
-initMainMenu();
-initHeroSlider();
-initToursSlider();
-initTrainingSlider();
