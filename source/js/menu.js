294514706fb6574697e5570d6d86f7df2643c91e
viktor <viktor-876@yandex.ru>
2024-08-07 20:02:06 +0300

    Добавляет JS для header

---
+++ source/js/menu.js
@@ -0,0 +1,53 @@
+
+const btnElement = document.querySelector('.main-header__menu-togle');
+const altBtnElement = document.querySelector('.main-header__menu-togle > span');
+const menuElement = document.querySelector('.main-nav');
+const listElement = document.querySelector('.main-nav__list');
+const pageBodyElement = document.querySelector('.page-body');
+
+
+
+function togleMenu() {
+
+  function MenuOpen() {
+    menuElement.classList.remove('main-nav--close');
+    btnElement.classList.add('main-header__menu-togle--open');
+    pageBodyElement.classList.add('page-body--no-scrol');
+
+    altBtnElement.innerHTML = 'закрыть меню.';
+
+    listElement.addEventListener('click', handleClickMenuItem)
+  }
+
+  function MenuClose() {
+    menuElement.classList.add('main-nav--close');
+    btnElement.classList.remove('main-header__menu-togle--open');
+    pageBodyElement.classList.remove('page-body--no-scrol');
+
+    altBtnElement.innerHTML = 'открыть меню.';
+
+    listElement.removeEventListener('click', handleClickMenuItem)
+  };
+
+
+
+  function handleClickTogler() {
+    if (menuElement.classList.contains('main-nav--close')) {
+      MenuOpen()
+    } else {
+      MenuClose()
+    }
+  };
+
+  function handleClickMenuItem(evt) {
+    if (evt.target.classList.contains('main-nav__link')) {
+      MenuClose()
+    } else {
+      return
+    }
+  }
+
+  btnElement.addEventListener('click', handleClickTogler);
+}
+
+export { togleMenu }
